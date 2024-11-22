001e71293c2422e93b2b8b2f47dac18688e4a801
Jack Lin <lin_jack@myyahoo.com>
2024-11-21 20:05:28 -0500

    Contact Page done

--- Portfolio/src/views/Home.vue
+++ Portfolio/src/views/Home.vue
@@ -1,8 +1,25 @@
 <script setup>
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <button className="btn">Button</button>
+    <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-lq rounded-sm shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Jack Lin</h1>
+      <p class="py-6">
+        Hello there! I'm Jack Lin, a full-stack developer based in New York City. 
+        I specialize in building web applications using modern technologies like Vue.js, React, and Node.js.
+      </p>
+      <RouterLink to="/contact">
+      <button class="btn btn-primary">Contact Me</button>
+        </RouterLink>
+    </div>
+  </div>
+</div>
 </template>
 
 <style scoped>
